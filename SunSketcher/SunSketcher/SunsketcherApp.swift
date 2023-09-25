8ec8d2ee7981abccb38b62ddfd933173d3014e5a
MikaMika2003 <143218826+MikaMika2003@users.noreply.github.com>
2023-09-25 12:43:35 -0500

    Add files via upload

---
+++ SunSketcher/SunSketcher/SunsketcherApp.swift
@@ -0,0 +1,20 @@
+//
+//  SunSketcherApp.swift
+//  SunSketcher
+//
+//  Created by ADMIN on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SunSketcherApp: App {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(locationManager)
+        }
+    }
+}
